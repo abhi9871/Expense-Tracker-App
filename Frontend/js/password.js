04942463c1d2c34817e507d3a4d07d623f81e618
abhi9871 <at719273@gmail.com>
2023-08-09 17:45:31 +0530

    Signup page

---
+++ Frontend/js/password.js
@@ -0,0 +1,38 @@
+let passwordInput = document.getElementById('password');
+let confirmPasswordInput = document.getElementById('password-confirm');
+let passwordLengthMsg = document.getElementById('passwordLengthMessage');
+// let submitButton = document.getElementById("submitButton");
+
+passwordInput.addEventListener('input', checkPasswordLength); // checks the length when user enters the password
+
+// Check password length
+function checkPasswordLength() {
+    let password = passwordInput.value;
+    if(password.length < 8 && password.length > 0){
+        passwordLengthMsg.textContent = 'Password must be atleast 8 characters long';
+        passwordLengthMsg.style.marginTop = '-10px';
+    }
+    else {
+        passwordLengthMsg.textContent = '';
+        passwordLengthMsg.style.marginTop = '0px';
+    }
+}
+
+// Check both the password fields to verify that user has entered the correct value or not
+
+confirmPasswordInput.addEventListener('input', confirmPassword);
+
+function confirmPassword() {
+    let password = passwordInput.value;
+    let confirmPwd = confirmPasswordInput.value;
+    if(password !== confirmPwd && confirmPwd !== ''){
+        confirmPasswordMessage.textContent = 'Password must be the same';
+        confirmPasswordMessage.style.marginTop = '-10px';
+    }
+    else {
+        confirmPasswordMessage.textContent = '';
+        confirmPasswordMessage.style.marginTop = '0px';
+    }
+}
+
+
