a2ba9bc28806437c7a56c8c70cd3acddf6bfa52e
abhi9871 <at719273@gmail.com>
2023-08-14 02:22:51 +0530

    Encryption code and minor css changes fixed

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,71 +1,57 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res, next) => {
-    const { name, email, password } = req.body;
-    try {
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds, async (err, hash) => {
-            if(err) {
-                console.log('Error hashing password:', err);
-                return;
-            }
-             // Store the hash in your password database
-        try {  
-        const user = await User.create({
-            name: name,
-            email: email,
-            password: hash
-        });
-        res.status(200).json({ success: true, message: "Sign up successful." });
-    } catch (err) {
-        if (err.name === "SequelizeValidationError") {
-            const validationErrors = {};
-            err.errors.forEach(error => {
-                validationErrors[error.path] = error.message;
-        });
-            res.status(400).json({ success: false, errors: validationErrors });
-        }
-        else if(err.name === "SequelizeUniqueConstraintError"){
-            res.status(400).json({ success: false, errors: {email: 'Email already exists'} });
-        }
-        else {
-            console.log(err);
-            res.status(500).json({ success: false, message: "Internal server error." });
-        }
-}
-})
-    } catch(err) {
-        console.error('Error:', err);
-        res.status(500).json({ success: false, message: 'Internal server error.' });
+  const { name, email, password } = req.body;
+  try {
+    if (password.length < 8) {
+      return res.status(400).json({success: false, errors: { password: "Password must be atleast 8 characters long" } });
     }
-}
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: hash,
+    });
+    res.status(200).json({ success: true, message: "Sign up successful." });
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      const validationErrors = {};
+      err.errors.forEach((error) => {
+        validationErrors[error.path] = error.message;
+      });
+      res.status(400).json({ success: false, errors: validationErrors });
+    } else if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(400).json({success: false, errors: { email: "Email already exists" },
+      });
+    } else {
+      console.log(err);
+      res.status(500).json({ success: false, message: "Internal server error." });
+    }
+  }
+};
 
 exports.loginUser = async (req, res, next) => {
-    const { email, password } = req.body;
-    try{
+  const { email, password } = req.body;
+  try {
     const user = await User.findOne({
-        where: {
-            email: email
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+        const result = await bcrypt.compare(password, user.password);
+        if(result){
+            res.status(200).json({ success: true, message: "User login successful!" });
         }
-    })
-    if(user){
-        bcrypt.compare(password, user.password, (err, result) => {
-            if(err) {
-                throw new Error('Something went wrong');
-            }
-            if(result){
-                res.json({ success: true, message: 'User login Successful!' });
-            }
-            if(user.password !== password){
-                res.json({ success: false, message: 'User not authorized' });
-            }
-    else {
-        res.json({ success: false, message: 'User does not exist' });
+        else {
+            res.status(401).json({ success: false, message: "User not authorized" });
+        }
+    } else {
+      res.status(404).json({ success: false, message: "User not found" });
     }
-})
-    }
-    } catch(err) {
-        res.status(500).json({ success: false, message: 'An error occurred' });
-    }
-}
+  } catch (err) {
+    res.status(500).json({ success: false, message: "An error occurred" });
+  }
+};
